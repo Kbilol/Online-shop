cedcdf72e408dcc2d0a8bb97ed0465670753df71
Freyzan2006 <freyzan2006@gmail.com>
2025-08-17 13:59:15 +0500

    Добавили Logo

---
+++ src/common/ui/Logo.tsx
@@ -0,0 +1,7 @@
+
+
+export const Logo: React.FC = () => {
+    return <div className="flex flex-col gap-3">
+        <img src="https://assets.turbologo.ru/blog/ru/2019/03/18161157/online-shop-logo-1.png" alt="" /> <h3>Online market</h3>
+    </div>
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import "./main.css"
+
 import App from './app/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
