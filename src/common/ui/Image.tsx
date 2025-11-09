3b17d9b7e82d54efd36b533acf05caa333d5bd97
Freyzan2006 <freyzan2006@gmail.com>
2025-11-09 14:57:40 +0500

    fix bag

--- src/common/ui/Image.tsx
+++ src/common/ui/Image.tsx
@@ -1,8 +1,10 @@
 
 
-interface IImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {}
 
-export const Image: React.FC<IImageProps> = ({ className, ...props }) => {
+
+export const Image: React.FC<
+React.ImgHTMLAttributes<HTMLImageElement>
+> = ({ className, ...props }) => {
     const base = "object-cover w-full h-full";
     const css = `${base} ${className}`;
 
