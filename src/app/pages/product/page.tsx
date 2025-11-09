ed5d47e084530a66d16783f658f4102aacb0cb7e
Freyzan2006 <freyzan2006@gmail.com>
2025-11-09 14:56:38 +0500

    added products to page of product

--- src/app/pages/product/page.tsx
+++ src/app/pages/product/page.tsx
@@ -1,5 +1,10 @@
+import { ProductTape } from "@product/index";
 
 
 export default function ProductPage() {
-    return <>Hello world !</>
+    return (
+        <section className="flex justify-center items-center gap-3 py-2">
+            <ProductTape />
+        </section>
+    )
 }
