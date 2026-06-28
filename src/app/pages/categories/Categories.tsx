01a364614b1e772a8efa6ddc934f19459aa40f11
Kbilol <qochqorovbilol27@gmail.com>
2026-06-28 13:43:56 +0500

    Добавил перевод

--- src/app/pages/categories/Categories.tsx
+++ src/app/pages/categories/Categories.tsx
@@ -1,15 +1,5 @@
 import { ListCategories } from "@category/widgets/list-categories";
-<<<<<<< HEAD
-
-
-export const Category = () => {
-  return (
-   <ListCategories />
-  );
-};
-=======
 
 export const Category = () => {
   return <ListCategories />;
 };
->>>>>>> main
