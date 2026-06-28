bba2b19a8b336fd7d4f64e799628a17f88af2811
Kbilol <qochqorovbilol27@gmail.com>
2026-06-28 13:48:45 +0500

    Добавил перевод

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./main.css";
 
+import "./i18n"; // ← обязательно
+
 import "flowbite";
 import "flowbite/dist/flowbite.css";
 
-import { AppRouter } from "./app/router/AppRouter.tsx";
-import { ServerStateProvider } from "@common/api/tanstack.provider.tsx";
+import { AppRouter } from "./app/router/AppRouter";
+import { ServerStateProvider } from "@common/api/tanstack.provider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
