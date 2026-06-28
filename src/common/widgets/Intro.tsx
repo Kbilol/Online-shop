5e9c7c7b45cdded41fb5ed4cbf7a52b528fe3480
Kbilol <qochqorovbilol27@gmail.com>
2026-06-28 13:24:06 +0500

    Добавил перевод

--- src/common/widgets/Intro.tsx
+++ src/common/widgets/Intro.tsx
@@ -2,12 +2,10 @@ import { Button } from "@common/ui/Button";
 import { Image } from "@common/ui/Image";
 import { Text } from "@common/ui/Text";
 import { Title } from "@common/ui/Title";
-import { useTranslate } from "@i18n/use-translate";
 import { useTranslation } from "react-i18next";
 
 export const Intro: React.FC = () => {
-  const { t, i18n } = useTranslation("product"); 
-
+  const { t } = useTranslation("product");
 
   return (
     <section className="flex mt-10 justify-center items-center gap-10 px-6 py-12 bg-gradient-to-r from-pink-100 via-white to-pink-50 rounded-2xl shadow-lg max-w-6xl mx-auto">
@@ -16,11 +14,12 @@ export const Intro: React.FC = () => {
         alt="Intro"
         className="w-[400px] rounded-2xl shadow-md hover:scale-105 transition-transform duration-500"
       />
-            <h1 className="color-black">{t( "Welcome" )} </h1>
-            <h2 className="color-black">{t( "productTitle" )} </h2>
-            <p>{t("product.description")}</p>
 
       <div className="flex flex-col gap-6 text-gray-700 max-w-xl">
+        <h1>{t("Welcome")}</h1>
+        <h2>{t("productTitle")}</h2>
+        <p>{t("description")}</p>
+
         <Title variant="secondary" size="lg">
           У нас вы найдёте всё необходимое <br />
           для себя и своих близких.
@@ -31,12 +30,12 @@ export const Intro: React.FC = () => {
           <span className="text-pink-500 font-semibold">
             качественные товары
           </span>{" "}
-          по доступным ценам, быструю доставку и удобный сервис. Каждый
-          покупатель для нас важен, поэтому мы заботимся о том, чтобы ваши
-          покупки были лёгкими и приятными.
+          по доступным ценам, быструю доставку и удобный сервис.
         </Text>
 
-        <Button variant="fourth">Перейти к покупкам</Button>
+        <Button variant="fourth">
+          Перейти к покупкам
+        </Button>
       </div>
     </section>
   );
